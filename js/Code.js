3aac7a2f1bf32e5a76af093b922dcd09d16033a8
SRIJAN891 <77321131+SRIJAN891@users.noreply.github.com>
2021-04-30 16:09:28 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode3 = "EDUCATION";
+const accessCode2 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+     fill("white")
+    textSize(15)
+    text("E S B A A A T D ", 600,150)
+    fill("lightblue")
+    text("Hint: IT STORES ALL THE INFORMATION !!", 600,170)
+
+     fill("white")
+    textSize(15)
+    text("A E I O U D C T N ",100,280)
+    fill("lightblue")
+    text("Hint: IT IS THE MOST POWERFUL WEAPON WHICH CAN CHANGE THE WORLD !!", 100,300)
+
+}
